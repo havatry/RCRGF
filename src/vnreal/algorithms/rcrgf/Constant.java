dbf0e0d96c52ac2f3999ea62b569cc7a895f04c0
hudedong <hudedong@corp.netease.com>
2020-04-18 00:14:13 +0800

    增加找到根节点和核心节点的步骤操作

---
+++ src/vnreal/algorithms/rcrgf/Constant.java
@@ -0,0 +1,9 @@
+package vnreal.algorithms.rcrgf;
+
+/**
+ * Created on 2020/4/17
+ * 定义一些常用常量
+ */
+public class Constant {
+
+}
--- src/vnreal/algorithms/rcrgf/Util.java
+++ src/vnreal/algorithms/rcrgf/Util.java
@@ -1,9 +1,16 @@
 package vnreal.algorithms.rcrgf;
 
 import vnreal.constraints.demands.BandwidthDemand;
+import vnreal.constraints.demands.CpuDemand;
 import vnreal.constraints.resources.BandwidthResource;
+import vnreal.constraints.resources.CpuResource;
 import vnreal.network.Link;
+import vnreal.network.Network;
+import vnreal.network.Node;
+import vnreal.network.substrate.SubstrateNode;
 import vnreal.network.virtual.VirtualLink;
+
+import java.util.Optional;
 
 /**
  * Created at 2020/4/10
@@ -36,7 +43,51 @@ public class Util {
         }
     }
 
+    /**
+     * 获取节点的cpu资源
+     * @param n 节点
+     * @return cpu资源值
+     */
+    public static double getCpu(Node<?> n) {
+        if (n instanceof SubstrateNode) {
+            return ((CpuResource)n.get().get(0)).getCycles() - ((CpuResource)n.get().get(0)).getOccupiedCycles();
+        } else {
+            return ((CpuDemand)n.get().get(0)).getDemandedCycles();
+        }
+    }
+
+
     public static int compareLinkBandwidth(Link l1, Link l2) {
         return Double.compare(getBandwidth(l1), getBandwidth(l2));
+    }
+
+    /**
+     * 选择网络中的核心节点，这类节点的特点是拥有的资源是最多的
+     * @param network 给定的网络拓扑
+     * @return 该网络这reference值最大的那个， 可能为null
+     */
+    public Optional<Node> selectCore(Network network) {
+        double referenceValue = -1.0;
+        Node select = null;
+        for (Object o : network.getVertices()) {
+            Node n = (Node) o;
+            double current = getReferenceValue(n, network);
+            if (current > referenceValue) {
+                referenceValue = current;
+                select = n;
+            }
+        }
+        return Optional.ofNullable(select);
+    }
+
+    // 计算给定节点的reference值
+    private double getReferenceValue(Node node, Network network) {
+        double bandwidth = 0.0;
+        for (Object o : network.getOutEdges(node)) {
+            // 强制转换为Link
+            Link l = (Link) o;
+            bandwidth += getBandwidth(l);
+        }
+        return getCpu(node) * bandwidth;
     }
 }
